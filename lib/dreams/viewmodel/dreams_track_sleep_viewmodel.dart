571c3f14a2f068cf07641954fa31a5cfbbd71653
Maggie Beach <beach214@d.umn.edu>
2023-03-22 17:08:58 -0500

    Changed sleep tracker functionality

--- lib/dreams/viewmodel/dreams_track_sleep_viewmodel.dart
+++ lib/dreams/viewmodel/dreams_track_sleep_viewmodel.dart
@@ -1,5 +1,77 @@
 
 
-class SleepTrackViewModel {
+import '../utils/dreams_constant.dart';
 
+class SleepTrackViewModel {
+  double _units = 0.0;
+  UnitType _unitType = UnitType.WAKE;
+  UnitType _unitTypeTime = UnitType.AM;
+
+  String _timeType = "";
+  String _message = "";
+
+  double bedHour = 0.0;
+  double bedMinute = 0.0;
+  double wakeHour = 0.0;
+  double wakeMinute = 0.0;
+  double sleepRating = 0.0;
+
+  double get units => _units;
+  set units(double outResult){
+    _units = outResult;
+  }
+
+  String get timeType => _timeType;
+  set timeType(String outResult){
+    _timeType = outResult;
+  }
+
+  String get message => _message;
+  set message(String outResult){
+    _message = outResult;
+  }
+
+  UnitType get unitTypeTime => _unitTypeTime;
+  set unitTypeTime(UnitType setValue){
+    _unitTypeTime = setValue;
+  }
+
+  UnitType get unitType => _unitType;
+  set unitType(UnitType setValue){
+    _unitType = setValue;
+  }
+
+  int get value => _unitType == UnitType.WAKE?0 : 1; //Set unit type - 1 is KPG
+  set value(int value){
+    if(value == 0){
+      _unitType = UnitType.WAKE;
+    } else {
+      _unitType = UnitType.BED;
+    }
+  }
+
+  int get valueWakeTime => _unitTypeTime == UnitType.AM?0 : 1; //Set unit type - 1 is KPG
+  set valueWakeTime(int value){
+    if(value == 0){
+      _unitTypeTime = UnitType.AM;
+    } else {
+      _unitTypeTime = UnitType.PM;
+    }
+  }
+  int get valueBedTime => _unitTypeTime == UnitType.AM?0 : 1; //Set unit type - 1 is KPG
+  set valueBedTime(int value){
+    if(value == 0){
+      _unitTypeTime = UnitType.AM;
+    } else {
+      _unitTypeTime = UnitType.PM;
+    }
+  }
+
+  String get timeInString => _timeType;
+  String get messageInString => _message;
+  String get resultInString => units.toStringAsFixed(2);
+  String get wakeHourInString => wakeHour != null ? wakeHour.toString():'';
+  String get wakeMinuteInString => wakeMinute != null ? wakeMinute.toString():'';
+
+  SleepTrackViewModel();
 }
--- lib/dreams/views/sleep_track_view/dreams_sleep_track_component.dart
+++ lib/dreams/views/sleep_track_view/dreams_sleep_track_component.dart
@@ -17,160 +17,31 @@ class TrackHomePage extends StatefulWidget{
 }
 
 class _TrackHomePageState extends State<TrackHomePage> implements TRACKView {
-  // Controllers for the text field widgets
-  final TextEditingController _sleepTimeController = TextEditingController();
-  final TextEditingController _wakeTimeController = TextEditingController();
 
-  String? _timeWentToBed;
-  String? _timeWokeUp;
-  final _formKey = GlobalKey<FormState>();
+  var _wakeHourController = TextEditingController();
+  var _wakeMinuteController = TextEditingController();
+  var _bedHourController = TextEditingController();
+  var _bedMinuteController = TextEditingController();
+  String _bedHour = "0.0";
+  String _bedMinute = "0.0";
+  String _wakeMinute = "0.0";
+  String _wakeHour = "0.0";
+  var _resultString = '';
+  var _timeString = '';
+  var _message = '';
+  var _value = 0;
+  var _valueBedTime = -1;
+  var _valueWakeTime = -1;
+  final FocusNode _bedHourFocus = FocusNode();
+  final FocusNode _wakeHourFocus = FocusNode();
+  final FocusNode _wakeMinuteFocus = FocusNode();
+  final FocusNode _bedMinuteFocus = FocusNode();
+
   late String average = 'Average Sleep: 0.0';
-
-  // Rating of quality of sleep
   double _sleepRating = 3;
 
 
-  // Regular expression for validating time input in format HH:MM am/pm
-  String? validateTime(String? input) {
-    final validTime = RegExp(r'^\d{1,2}:\d{2} (am|pm)$');
-    if (!validTime.hasMatch(input ?? '')) {
-      return 'Please enter a valid time (e.g. 3:30 pm)';
-    }
-    return null;
-  }
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save(); // Save the form values
-      setState(() {
-        average = this.widget.trackPresenter.onSubmitClicked(_timeWentToBed!, _timeWokeUp!, _sleepRating)!;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sleep Tracker'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 16.0), // add 16 pixels of spacing
-            // Row for inputting sleep and wake times
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // Text field for inputting sleep time
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: _sleepTimeController,
-                      decoration: InputDecoration(
-                        labelText: 'Time Went To Bed',
-                        border: OutlineInputBorder(),
-                      ),
-                      keyboardType: TextInputType.datetime,
-                      inputFormatters: [],
-                      validator: validateTime,
-                      onSaved: (String? value) {
-                        _timeWentToBed = value ?? '';
-                      },
-                    ),
-                  ),
-                ),
-
-                // Text field for inputting wake time
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: _wakeTimeController,
-                      decoration: InputDecoration(
-                        labelText: 'Time You Woke Up',
-                        border: OutlineInputBorder(),
-                      ),
-                      keyboardType: TextInputType.datetime,
-                      inputFormatters: [],
-                      validator: validateTime,
-                      onSaved: (String? value) {
-                        _timeWokeUp = value ?? '';
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16.0), // add 16 pixels of spacing
-            // Button to enter sleep data
-            ElevatedButton(
-              onPressed: () {
-                _submitForm();
-                setState(() {});
-              },
-              child: Text('Submit'),
-            ),
-            SizedBox(height: 32.0), // add 16 pixels of spacing
-            Text(
-              'Rate your quality of sleep',
-              style: TextStyle(color: Colors.blueAccent.shade700),
-            ),
-            // Slider for rating quality of sleep
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Bad (1)'),
-                  Text('Good (5)'),
-                ],
-              ),
-               Slider(
-                value: _sleepRating,
-                min: 1,
-                max: 5,
-                divisions: 4,
-                onChanged: (double value) {
-                  setState(() {
-                    _sleepRating = value;
-                  });
-                },
-                label: '$_sleepRating',
-              ),
-              ],
-            ),
-          ),
-        Padding(
-          padding: const EdgeInsets.all(32.0),
-            // Text widget to display average sleep
-              child: Container(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                    average,
-                  //_calculateAverageSleep(), need to create this function
-                  //style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
-                  return SleepDataScreen();
-                }));
-              },
-              child: Text('Enter sleep data'),
-            ),
-        ],
-      ),
-    )
-    );
-  }
-
+  var _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -178,10 +49,369 @@ class _TrackHomePageState extends State<TrackHomePage> implements TRACKView {
     this.widget.trackPresenter.trackView = this;
   }
 
-  set trackView(TRACKView value) {
+  void handleRadioValueChanged(int? value) {
+    this.widget.trackPresenter.onOptionChanged(value!, wakeHourString: _wakeHour, wakeMinuteString: _wakeMinute );
   }
-}
+  void handleRadioValueChangedBedTime(int? value) {
+    this.widget.trackPresenter.onBedTimeOptionChanged(value!);
+  }
+  void handleRadioValueChangedWakeTime(int? value) {
+    this.widget.trackPresenter.onWakeTimeOptionChanged(value!);
+  }
 
+  void _submission() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      this.widget.trackPresenter.onSubmitClicked(_bedHour, _bedMinute, _wakeMinute, _wakeHour, _sleepRating);
+    }
+  }
+
+
+
+
+    @override
+  void updateResultValue(String resultValue){
+    setState(() {
+      _resultString = resultValue;
+    });
+  }
+  @override
+  void updateTimeString(String timeString){
+    setState(() {
+      _timeString = timeString;
+    });
+  }
+  @override
+  void updateMessage(String message){
+    setState(() {
+      _message = message;
+    });
+  }
+  @override
+  void updateWakeMinute({required String wakeMinute}){
+    setState(() {
+      // ignore: unnecessary_null_comparison
+      _wakeMinuteController.text = wakeMinute != null?wakeMinute:'';
+    });
+  }
+  @override
+  void updateWakeHour({required String wakeHour}){
+    setState(() {
+      // ignore: unnecessary_null_comparison
+      _wakeHourController.text = wakeHour != null?wakeHour:'';
+    });
+  }
+  @override
+  void updateBedHour({required String bedHour}) {
+    setState(() {
+      _bedHourController.text = bedHour != null ? bedHour : '';
+    });
+  }
+  @override
+  void updateBedMinute({required String bedMinute}) {
+    setState(() {
+      _bedMinuteController.text = bedMinute != null ? bedMinute : '';
+    });
+  }
+  @override
+  void updateUnit(int value){
+    setState(() {
+      _value = value;
+    });
+  }
+  @override
+  void updateBedTimeUnit(int value){
+    setState(() {
+      _valueBedTime = value;
+    });
+  }
+  @override
+  void updateWakeTimeUnit(int value){
+    setState(() {
+      _valueWakeTime = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    var _unitViewBedTime = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Radio<int>(
+          activeColor: Colors.blueAccent.shade700,
+          value: 0, groupValue: _valueBedTime, onChanged: handleRadioValueChangedBedTime,
+        ),
+        Text(
+          'AM',
+          style: TextStyle(color: Colors.blueAccent.shade700),
+        ),
+        Radio<int>(
+          activeColor: Colors.blueAccent.shade700,
+          value: 1, groupValue: _valueBedTime, onChanged: handleRadioValueChangedBedTime,
+        ),
+        Text(
+          'PM',
+          style: TextStyle(color: Colors.blueAccent.shade700),
+        ),
+      ],
+    );
+
+    var _unitViewWakeTime = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Radio<int>(
+          activeColor: Colors.blueAccent.shade700,
+          value: 0, groupValue: _valueWakeTime, onChanged: handleRadioValueChangedWakeTime,
+        ),
+        Text(
+          'AM',
+          style: TextStyle(color: Colors.blueAccent.shade700),
+        ),
+        Radio<int>(
+          activeColor: Colors.blueAccent.shade700,
+          value: 1, groupValue: _valueWakeTime, onChanged: handleRadioValueChangedWakeTime,
+        ),
+        Text(
+          'PM',
+          style: TextStyle(color: Colors.blueAccent.shade700),
+        ),
+      ],
+    );
+
+    var _mainPartView = Container(
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 6.0),
+                child: Text("I went to bed at:",style: const TextStyle(fontWeight: FontWeight.bold), textScaleFactor: 1.5,)
+                ,),
+              //_unitView,
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: bedHourFormField(context),
+                  ),
+                  Expanded(
+                    child: bedMinFormField(context),
+                  )
+                ],
+              ),
+              _unitViewBedTime,
+              Padding(
+                padding: EdgeInsets.only(top: 6.0),
+                child: Text("I woke up at:",style: const TextStyle(fontWeight: FontWeight.bold), textScaleFactor: 1.5,)
+                ,),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: wakeHourFormField(context),
+                  ),
+                  Expanded(
+                    child: wakeMinuteFormField(),
+                  ),
+                ],
+              ),
+              _unitViewWakeTime,
+            ],
+          ),
+        ),
+      ),
+    );
+
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Sleep Tracker'),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent.shade700,
+        ),
+        backgroundColor: Colors.white,
+        body: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(4.0)),
+            _mainPartView,
+            /*Padding(padding: EdgeInsets.all(4.0)),
+            _resultView,*/
+          Text(
+            'Rate your quality of sleep',
+            style: TextStyle(color: Colors.blueAccent.shade700, fontSize: 16.0),
+          ),
+
+          // Slider for rating quality of sleep
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Bad (1)'),
+                    Text('Good (5)'),
+                  ],
+                ),
+                Slider(
+                  value: _sleepRating,
+                  min: 1,
+                  max: 5,
+                  divisions: 4,
+                  onChanged: (double value) {
+                    setState(() {
+                      _sleepRating = value;
+                    });
+                  },
+                  label: '$_sleepRating',
+                ),
+              ],
+            ),
+          ),
+            Padding(
+              padding: EdgeInsets.only(top: 6.0),
+              child: submitButton()
+              ,),
+
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+                return SleepDataScreen();
+              }));
+            },
+            child: Text('Next: Enter sleep data'),
+          ),
+          ],
+        ),
+    );
+  }
+
+
+  ElevatedButton submitButton() {
+    return ElevatedButton(
+      onPressed: _submission,
+      style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blueAccent.shade700,
+          textStyle: TextStyle(color: Colors.white70)
+      ),
+      child: Text(
+        'Submit',
+        style: TextStyle(fontSize: 16.0),
+      ),
+    );
+  }
+
+  TextFormField wakeMinuteFormField() {
+    return TextFormField(
+      controller: _wakeMinuteController,
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.done,
+      focusNode: _wakeMinuteFocus,
+      onFieldSubmitted: (value){
+        _wakeMinuteFocus.unfocus();
+      },
+      validator: (value) {
+        if (value!.length == 0 || (double.parse(value) < 0 || double.parse(value) > 59)) {
+          return ('Minute between 0 - 59');
+        }
+      },
+      onSaved: (value) {
+        _wakeMinute = value!;
+      },
+      decoration: InputDecoration(
+          hintText: 'e.g.) 40',
+          labelText: 'Minute',
+          icon: Icon(Icons.assessment),
+          fillColor: Colors.white
+      ),
+    );
+  }
+
+  TextFormField wakeHourFormField(BuildContext context) {
+    return TextFormField(
+      controller: _wakeHourController,
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.next,
+      focusNode: _wakeHourFocus,
+      onFieldSubmitted: (term) {
+        _fieldFocusChange(context, _wakeHourFocus, _wakeMinuteFocus);
+      },
+      validator: (value) {
+        if (value!.length == 0 || (double.parse(value) < 1 || double.parse(value) > 12)) {
+          return ('Hour between 1 - 12');
+        }
+      },
+      onSaved: (value) {
+        _wakeHour = value!;
+      },
+      decoration: InputDecoration(
+        hintText: "e.g.) 7",
+        labelText: "Hour",
+        icon: Icon(Icons.assessment),
+        fillColor: Colors.white,
+      ),
+    );
+  }
+
+  TextFormField bedHourFormField(BuildContext context) {
+    return TextFormField(
+      controller: _bedHourController,
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.next,
+      focusNode: _bedHourFocus,
+      onFieldSubmitted: (term){
+        _fieldFocusChange(context, _bedHourFocus, _bedMinuteFocus);
+      },
+      validator: (value) {
+        if (value!.length == 0 || (double.parse(value) < 1 || double.parse(value) > 12)) {
+          return ('Hour between 1 - 12');
+        }
+      },
+      onSaved: (value) {
+        _bedHour = value!;
+      },
+      decoration: InputDecoration(
+        hintText: 'e.g.) 6',
+        labelText: 'Hour',
+        icon: Icon(Icons.assessment),
+        fillColor: Colors.white,
+      ),
+    );
+  }
+
+  TextFormField bedMinFormField(BuildContext context) {
+    return TextFormField(
+      controller: _bedMinuteController,
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.next,
+      focusNode: _bedMinuteFocus,
+      onFieldSubmitted: (term){
+        _fieldFocusChange(context, _bedMinuteFocus, _wakeHourFocus);
+      },
+      validator: (value) {
+        if (value!.length == 0 || (double.parse(value) < 0 || double.parse(value) > 59)) {
+          return ('Minute between 0 - 59');
+        }
+      },
+      onSaved: (value) {
+        _bedMinute = value!;
+      },
+      decoration: InputDecoration(
+        hintText: 'e.g.) 30',
+        labelText: 'Minute',
+        icon: Icon(Icons.assessment),
+        fillColor: Colors.white,
+      ),
+    );
+  }
+
+  _fieldFocusChange(BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+
+}
 class SleepDataScreen extends StatefulWidget {
   @override
   _SleepDataScreen createState() => _SleepDataScreen();
