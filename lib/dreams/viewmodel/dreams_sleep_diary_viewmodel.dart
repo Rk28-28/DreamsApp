17423c22436fbdcbe9bddd9fc4d11df2cf96c5e6
wegle039 <wegle039@d.umn.edu>
2023-04-08 15:40:58 -0500

    Testing retrieval for diary entries

--- lib/dreams/viewmodel/dreams_sleep_diary_viewmodel.dart
+++ lib/dreams/viewmodel/dreams_sleep_diary_viewmodel.dart
@@ -1,4 +1,6 @@
 //import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:stack/stack.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -64,13 +66,33 @@ int count = 0;
       print(counter);
       print(val); //could store values into an array/list, and  run a for loop and send the 5 most recent entries.
     });*/
-
+    
     Query query = databaseRefDiary.limitToLast(2);
     DataSnapshot event = await query.get();
+    
+    String data = event.value.toString();
+
+    var entriesToPrint = ['', '', '', '', ''];
+    int commaPos = 0;
+    int entryPos = 0;
+
+    data = data.substring(2, data.length - 2); //Cuts off [{ ... }] at the end of the string
+    for(int i = 0; i < data.length; ++i)
+      {
+        if(entryPos >= 5)
+          break;
+        if(data.substring(i, i+1) == ",")
+          {
+            entriesToPrint[entryPos] = data.substring(commaPos + 1, i-1);
+            commaPos = i;
+            entryPos++;
+          }
+      }
+
 
     //event.count;
+for(String entry in entriesToPrint) {print(entry);}
 
-    print(event.value);
   }
 }
 
