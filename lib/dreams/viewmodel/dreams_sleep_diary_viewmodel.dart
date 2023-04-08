e7e4e4379ca05f68e94344988f67b0f2900a9580
Ritvik Kosuri <kosur005@umn.edu>
2023-04-08 14:27:08 -0500

    Update dreams_sleep_diary_viewmodel.dart

--- lib/dreams/viewmodel/dreams_sleep_diary_viewmodel.dart
+++ lib/dreams/viewmodel/dreams_sleep_diary_viewmodel.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -10,7 +10,7 @@ class SleepDiaryViewModel {
   FirebaseAuth auth = FirebaseAuth.instance;
 
   String diaryEntry = "";
-
+int count = 0;
   getuid() {
     final User? user = auth.currentUser;
     final uid = user?.uid;
@@ -31,18 +31,47 @@ class SleepDiaryViewModel {
     };
 
     databaseRefDiary.update(diaryData);
+    ++count;
   }
 
 
   Future<void> loadUserData() async {
     final x = getuid();
     DatabaseReference databaseRefDiary = FirebaseDatabase.instance.ref(
-        'users/$x/');
-    await databaseRefDiary.child("diary-entries").get().then((event) {
-      print(event.value); //could store values into an array/list, and  run a for loop and send the 5 most recent entries.
+        'users/$x/diary-entries/');
+    //List val = [];
+    //int counter =0;
+   // List<String> locations = [];
+    //final data = await databaseRefDiary.child("diary-entries").get();
+
+    /*data.forEach((element) {
+      final map2 = element.data();
+      locations.add(map2[counter]);
+      ++counter;
+      });
+    print(locations);*/
+
+   /* await FirebaseFirestore.instance.collection('users/$x/diary-entries').get().then((event) {
+      event.docs.forEach((doc) {
+        locations.add(doc.data() as String);
+      });
     });
+    print(locations);*/
+      //could store values into an array/list, and  run a for loop and send the 5 most recent entries.
+    /*await databaseRefDiary.child("diary-entries").get().then((event) {
+     event.value; //this stores all entries into 1 element, instead of a seperate element.
+      ++counter;
+      print(counter);
+      print(val); //could store values into an array/list, and  run a for loop and send the 5 most recent entries.
+    });*/
+
+    Query query = databaseRefDiary.limitToLast(2);
+    DataSnapshot event = await query.get();
+
+    //event.count;
+
+    print(event.value);
   }
 }
-
 
 
