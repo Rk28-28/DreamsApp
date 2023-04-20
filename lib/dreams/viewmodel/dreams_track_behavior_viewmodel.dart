65eadddd3da94e66e008e9fe1493a0674a9196a0
Ritvik Kosuri <kosur005@umn.edu>
2023-04-19 20:23:13 -0500

    Update dreams_track_behavior_viewmodel.dart

--- lib/dreams/viewmodel/dreams_track_behavior_viewmodel.dart
+++ lib/dreams/viewmodel/dreams_track_behavior_viewmodel.dart
@@ -26,7 +26,7 @@ class BehaviorTrackViewModel {
 
     sleepBehaviorRef.set({
       'Calories': calories,
-      'Caffeine': caffeineconsumpted,
+      'Caffeine': caffeineconsumpted + " mg",
       'Mood': moodval
     });
   }
