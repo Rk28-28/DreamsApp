5c7bdb7d81443099eab951241dca45c04bc2f60f
Maggie Beach <beach214@d.umn.edu>
2023-04-24 13:33:43 -0500

    Changed buttons/layout on white noise page

--- lib/dreams/views/extra_info_views/ButtonPages/white_noise_component.dart
+++ lib/dreams/views/extra_info_views/ButtonPages/white_noise_component.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:units/dreams/views/extra_info_views/MoonButton.dart';
 
 import '../MyWebView.dart';
-import '../CloudButtonEI.dart';
 
 class WhiteNoiseHomePage extends StatefulWidget{
 
@@ -30,77 +30,81 @@ class _WhiteNoiseHomePageState extends State<WhiteNoiseHomePage> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Column (
-            children: <Widget>[
-              Container(
-                  height: 600,
-                  child : Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Center (
-                                  child: CloudButton (
-                                    text: "White Noise",
-                                    onPressed: () {
-                                      _url = 'https://www.youtube.com/watch?v=wzjWIxXBs_s';
-                                      _navigateToWebView(context, _url);
-                                    },
-                                    imageAsset: 'assets/cloud-clipart-md.png',
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Center (
-                                  child: CloudButton (
+          child: Column (
+              children: <Widget>[
+                Container(
+                    height: 650,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: MoonButton (
+                              text: "White Noise",
+                              onPressed: () {
+                                _url = 'https://www.youtube.com/watch?v=wzjWIxXBs_s';
+                                _navigateToWebView(context, _url);
+                              },
+                              imageAsset: 'assets/greenplanet.png',
+                            ),
+                          ),
+
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children:[
+                                SizedBox(
+                                  height: 140,
+                                  width: 140,
+                                  child: MoonButton (
                                     text: "Brown Noise",
                                     onPressed: () {
                                       _url = 'https://www.youtube.com/watch?v=DunIRy0PPCs';
                                       _navigateToWebView(context, _url);
                                     },
-                                    imageAsset: 'assets/cloud-clipart-md.png',
+                                    imageAsset: 'assets/greenplanet.png',
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Center (
-                                  child: CloudButton (
+                                SizedBox(
+                                  height: 140,
+                                  width: 140,
+                                  child: MoonButton (
                                     text: "Ambient Music",
                                     onPressed: () {
                                       _url = 'https://www.youtube.com/watch?v=SqqMS8QBPPE';
                                       _navigateToWebView(context, _url);
                                     },
-                                    imageAsset: 'assets/cloud-clipart-md.png',
+                                    imageAsset: 'assets/greenplanet.png',
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: Center (
-                                  child: CloudButton (
-                                    text: "Rain Sounds",
-                                    onPressed: () {
-                                      _url = 'https://www.youtube.com/watch?v=QAINEz_TFvs';
-                                      _navigateToWebView(context, _url);
-                                    },
-                                    imageAsset: 'assets/cloud-clipart-md.png',
-                                  ),
+                              ]
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Center (
+                              child: SizedBox(
+                                height: 140,
+                                width: 140,
+                                child: MoonButton (
+                                  text: "Rain Sounds",
+                                  onPressed: () {
+                                    _url = 'https://www.youtube.com/watch?v=QAINEz_TFvs';
+                                    _navigateToWebView(context, _url);
+                                  },
+                                  imageAsset: 'assets/greenplanet.png',
                                 ),
                               ),
-                            ]
-                        )
-                      ]
-                  )
-              )
-            ]
-        ),
+                            ),
+                          ),
+                        ]
+                    )
+                )
+              ]
+          ),
       ),
     );
   }
+
 
 
   @override
